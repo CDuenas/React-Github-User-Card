44c79cc24c67063ca7a9d8947d1284c2f59a15bb
Christian <Chrisaduenas@gmail.com>
2020-02-14 18:24:09 -0800

    touchups

--- react-github-user-card/src/components/ProfileCard.js
+++ react-github-user-card/src/components/ProfileCard.js
@@ -1,18 +1,19 @@
 import React from "react";
 import {
     Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button
+    CardTitle, CardSubtitle, Col, Row
 } from 'reactstrap';
 
 const ProfileCard = props => {
     return (
-        <Card body outline color="secondary">
-            <CardImg height="100" src={props.image} alt="Profile Avatar" />
-            <CardBody >
+        <div>
+            <Card outline color="secondary">
+                <CardImg height="150" src={props.image} alt="Profile Avatar" />
                 <CardTitle>Name: {props.name}</CardTitle>
                 <CardSubtitle>Location: {props.location}</CardSubtitle>
-            </CardBody>
-        </Card>
+            </Card>
+        </div>
+
     )
 }
 
